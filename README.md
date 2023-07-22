031cd80892d78d8a777c8e079fe5d4fb6ee6a525
yt <stonehamjp@gmail.com>
2023-07-22 23:38:43 +0900

    Initial commit

---
+++ README.md
@@ -0,0 +1,19 @@
+# tweet-weather-and-news
+
+## Description
+This is a Python script that tweets the weather and news headlines for Tokyo.
+
+It uses the [Free Weather API](https://open-meteo.com/) and the [News API](https://newsapi.org/) to get the weather and news headlines respectively.
+
+It uses the [ChatGPT API](https://openai.com/) to generate a tweet.
+
+It then tweets the weather and news headlines using the [Tweepy API](https://www.tweepy.org/).
+
+## Installation
+1. Clone this repository.
+2. Install the dependencies using `pip install -r requirements.txt`.
+3. Create a `.env` file with the following command `cp .env.default .env` in the root directory of this repository.
+4. Fill in the `.env` file with your API keys.
+
+## Usage
+Run the script using `python main.py`.
---
+++ main.py
@@ -0,0 +1,103 @@
+import openai
+import requests
+import tweepy
+import os
+from dotenv import load_dotenv
+
+# 実行前に下記の項目を .env に記載する
+# NEWS_API_KEY=
+# OPENAI_ORGANIZATION=
+# OPENAI_API_KEY=
+# TWITTER_CONSUMER_KEY=
+# TWITTER_CONSUMER_SECRET=
+# TWITTER_ACCESS_TOKEN=
+# TWITTER_ACCESS_TOKEN_SECRET=
+
+# 天気を取得する
+def fetch_weather():
+    meteo_url = "https://api.open-meteo.com/v1/forecast?latitude=35.6895&longitude=139.6917&daily=temperature_2m_max," \
+                "temperature_2m_min,apparent_temperature_max,apparent_temperature_min,sunrise," \
+                "sunset&timezone=Asia%2FTokyo"
+    return requests.get(meteo_url).json()
+
+
+# ニュースを取得する
+def fetch_news():
+    news_api_key = os.environ.get("NEWS_API_KEY")
+    news_api_url = "https://newsapi.org/v2/top-headlines?country=jp&category=business&pageSize=3&apiKey=" + news_api_key
+    return requests.get(news_api_url).json()
+
+
+# プロンプトを実行する
+def execute_prompt(prompt):
+    openai.organization = os.environ.get("OPENAI_ORGANIZATION")
+    openai.api_key = os.environ.get("OPENAI_API_KEY")
+    completion = openai.ChatCompletion.create(
+        model="gpt-3.5-turbo",
+        messages=[{"role": "user", "content": prompt}]
+    )
+    return completion["choices"][0]["message"]["content"]
+
+
+def create_prompt(temp_max, temp_min, news_title):
+    return """
+    下記の情報とルールからtwitterに投稿する文章を考えて下さい。
+    
+    【情報】
+    ・今日の東京は最高気温「{}℃」、最低気温「{}℃」である
+    ・直近のビジネスニュースは下記である
+    {}
+    
+    【ルール】
+    ・投稿は必ず140文字以内に収めること
+    ・ポジティブな形で伝えること
+    ・アナウンサーが読むような形で伝えること
+    ・ですます調で伝えること
+    ・ニュースの内容は重要なものだけ取り上げて、不要と判断した場合は投稿に含めなくて良い
+    ・文章の先頭と末尾に " や 「」 などの記号は入れないこと
+    ・気温に関して、暑いか寒いなどの表現を入れること
+    ・ツイートの文章以外は、文章の中に入れないこと
+    """.format(
+        temp_max,
+        temp_min,
+        news_title,
+    )
+
+
+# 4. ツイートする
+def tweet(text):
+    consumer_key = os.environ.get("TWITTER_CONSUMER_KEY")
+    consumer_secret = os.environ.get("TWITTER_CONSUMER_SECRET")
+    access_token = os.environ.get("TWITTER_ACCESS_TOKEN")
+    access_token_secret = os.environ.get("TWITTER_ACCESS_TOKEN_SECRET")
+
+    client = tweepy.Client(
+        consumer_key=consumer_key,
+        consumer_secret=consumer_secret,
+        access_token=access_token,
+        access_token_secret=access_token_secret
+    )
+    client.create_tweet(text=text)
+
+
+def main():
+    # 0. 環境変数を読み込む
+    load_dotenv()
+    # 1. 天気を取得する
+    weather = fetch_weather()
+    # 2. ニュースを取得する
+    news = fetch_news()
+    # 3. ツイートの文章を生成するプロンプトを作成する
+    prompt = create_prompt(
+        weather["daily"]["temperature_2m_max"][0],
+        weather["daily"]["temperature_2m_min"][0],
+        news["articles"][0]["title"],
+    )
+    # 4. chatgptでプロンプトから文章を生成する
+    text = execute_prompt(prompt)
+    # 5. ツイートする（chatgptが140文字以内に収めないケースがあるため、念のため切り捨てる）
+    tweet(text[0:140])
+
+
+if __name__ == "__main__":
+    main()
